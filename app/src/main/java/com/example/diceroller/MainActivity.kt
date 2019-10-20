83f46b525aeec95a867d0238cb6de84f96ebbf14
Jarrod <lowjar20@gmail.com>
2019-10-20 22:44:41 +0800

    Updated v2 for the dice count up
    updated added all count up for all the dice

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -14,8 +14,11 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        //Setting target to be assign
         val rollButton: Button = findViewById(R.id.roll_button)
         val countButton: Button = findViewById(R.id.count_button)
+
+        //Action for dice
         rollButton.setOnClickListener { rollDice() }
         countButton.setOnClickListener { countDice() }
         }
@@ -41,16 +44,26 @@ class MainActivity : AppCompatActivity() {
     private fun countDice() {
 
 
-
+        //Getting Dice Random Value
         val resultText: TextView = findViewById(R.id.result_text)
         val resultText1: TextView = findViewById(R.id.result1_text)
         val resultText2: TextView = findViewById(R.id.result2_text)
 
+        //Convert Dice Random Value to Integer for + 1
         val countText: TextView = findViewById(R.id.result_count)
-        val totalText: TextView = findViewById(R.id.result1_count)
+        val countText1: TextView = findViewById(R.id.result1_count)
+        val countText2: TextView = findViewById(R.id.result2_count)
+        val totalText: TextView = findViewById(R.id.total_count)
 
+        //Plus 1 for the dice Value
         val result = Integer.parseInt(resultText.text.toString()) + 1
+        val result1 = Integer.parseInt(resultText1.text.toString()) + 1
+        val result2 = Integer.parseInt(resultText2.text.toString()) + 1
+
+        //Calculation for the total dice Value
         val total = Integer.parseInt(resultText.text.toString()) + Integer.parseInt(resultText1.text.toString()) + Integer.parseInt(resultText2.text.toString())
+
+        //dice1
         if( result >= 6)
         {
             countText.text = "6"
@@ -59,7 +72,25 @@ class MainActivity : AppCompatActivity() {
         else{
             countText.text = result.toString()
         }
+        //dice 2
+        if( result1 >= 6)
+        {
+            countText1.text = "6"
 
+        }
+        else{
+            countText1.text = result1.toString()
+        }
+        //dice 3
+        if( result2 >= 6)
+        {
+            countText2.text = "6"
+
+        }
+        else{
+            countText2.text = result2.toString()
+        }
+        //Total Dice Amount
         if(total >=18)
         {
             totalText.text = "18"
